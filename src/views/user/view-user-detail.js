2875fd4c6e9119d476ce9327bd821eba5d7c5c15
sage-nilton <nilton.rodrigues@sage.com>
2019-12-05 16:14:22 -0300

    ...

--- src/views/user/view-user-detail.js
+++ src/views/user/view-user-detail.js
@@ -6,6 +6,7 @@ import CustomInput from '../../component/buttons-label/custom-input';
 import CustomCheckbox from  '../../component/buttons-label/custom-checkbox';
 import CustomRadioButton from '../../component/buttons-label/custom-radiobutton';
 import CustomButton from '../../component/buttons-label/custom-button';
+import LoadingOverlay from 'react-loading-overlay';
 import './user.scss';
 
 const style = {
@@ -24,6 +25,7 @@ class ViewUserDetail extends React.Component{
     constructor(props){
         super(props)
         this.state = {
+            isActive: false,
             Username: "",
             Name: "",
             Email: "",
@@ -78,80 +80,94 @@ class ViewUserDetail extends React.Component{
     render(){
 
         return (
-            <React.Fragment>
-                <HeaderRegistration/>
-                <HorizontalBar/>
-                <div className="container">
-                    <div className="block-conteiner-one">
-                        <div>
-                            <CustomLabel>Username</CustomLabel>
-                            <CustomInput onChange={ (e)=> this.handleChangeValue("Username", e.target.value) } value={ this.state.Username } />
-                        </div>
-                        <div>
-                            <CustomLabel>Name</CustomLabel>
-                            <CustomInput onChange={ (e)=> this.handleChangeValue("Name", e.target.value) } value={ this.state.Name }/>
-                        </div>
-                        <div>
-                            <CustomLabel>E-mail</CustomLabel>
-                            <CustomInput onChange={ (e)=> this.handleChangeValue("Email", e.target.value) } value={ this.state.Email }/>
-                        </div>
+            <LoadingOverlay
+                active={this.state.isActive}
+                spinner
+                text='Wait please...'
+            >
+                <React.Fragment>
+                    <HeaderRegistration/>
+                    <HorizontalBar/>
+                    <div className="container">
+                        <div className="block-conteiner-one">
+                            <div>
+                                <CustomLabel>Username</CustomLabel>
+                                <CustomInput onChange={ (e)=> this.handleChangeValue("Username", e.target.value) } value={ this.state.Username } />
+                            </div>
+                            <div>
+                                <CustomLabel>Name</CustomLabel>
+                                <CustomInput onChange={ (e)=> this.handleChangeValue("Name", e.target.value) } value={ this.state.Name }/>
+                            </div>
+                            <div>
+                                <CustomLabel>E-mail</CustomLabel>
+                                <CustomInput onChange={ (e)=> this.handleChangeValue("Email", e.target.value) } value={ this.state.Email }/>
+                            </div>
 
-                        <CustomButton onClick={ (e)=> {
-                            e.preventDefault();
-                            this.props.onPostUser(this.state)}}
-                        >
-                            Save
-                        </CustomButton>
-                        <CustomButton
-                            secundary={ true }
-                            onClick={ ()=> this.props.history.push('/user_list/') }
-                        >
-                            Dischard
-                        </CustomButton>
+                            <CustomButton onClick={ (e)=> {
+                                e.preventDefault();
+                                this.setState({...this.state, isActive: true})
+                                this.props.onPostUser(this.state)
+                                    .then(()=>{
+                                        this.setState({...this.state, isActive: true})
+                                    })
+                                    .catch(()=>{
+                                        this.setState({...this.state, isActive: true})
+                                    })
+                            }}
+                            >
+                                Save
+                            </CustomButton>
+                            <CustomButton
+                                secundary={ true }
+                                onClick={ ()=> this.props.history.push('/user_list/') }
+                            >
+                                Dischard
+                            </CustomButton>
 
+                        </div>
+                        <div className="block-conteiner-two">
+                            <div>
+                                <CustomLabel>City</CustomLabel>
+                                <CustomInput onChange={ (e)=> this.handleChangeValue("City", e.target.value) } value={ this.state.City }/>
+                            </div>
+
+                            <div className="display-checkbox">
+                                <CustomRadioButton title="Aways" checked={ this.state.Aways } onChange={(e)=> this.handleChangeValue("Aways", e.target.checked) } />
+                            </div>
+                            <div className="display-checkbox">
+                                <CustomRadioButton title="Sometimes" checked={ this.state.Sometimes } onChange={(e)=> this.handleChangeValue("Sometimes", e.target.checked) }/>
+                            </div>
+                            <div className="display-checkbox">
+                                <CustomRadioButton title="Never" checked={ this.state.Never } onChange={(e)=> this.handleChangeValue("Never", e.target.checked) }/>
+                            </div>
+
+                            <CustomLabel>Days of the week</CustomLabel>
+                            <div className="display-checkbox">
+                                <CustomCheckbox title="Sun" checked={ this.state.Sun } onChange={(e)=> this.handleChangeValue("Sun", e.target.checked) }/>
+                            </div>
+                            <div className="display-checkbox">
+                                <CustomCheckbox title="Mon" checked={ this.state.Mon } onChange={(e)=> this.handleChangeValue("Mon", e.target.checked) }/>
+                            </div>
+                            <div className="display-checkbox">
+                                <CustomCheckbox title="Tue" checked={ this.state.Tue } onChange={(e)=> this.handleChangeValue("Tue", e.target.checked) }/>
+                            </div>
+                            <div className="display-checkbox">
+                                <CustomCheckbox title="Wed" checked={ this.state.Wed } onChange={(e)=> this.handleChangeValue("Wed", e.target.checked) }/>
+                            </div>
+                            <div className="display-checkbox">
+                                <CustomCheckbox title="Thu" checked={ this.state.Thu } onChange={(e)=> this.handleChangeValue("Thu", e.target.checked) }/>
+                            </div>
+                            <div className="display-checkbox">
+                                <CustomCheckbox title="Fri" checked={ this.state.Fri } onChange={(e)=> this.handleChangeValue("Fri", e.target.checked) }/>
+                            </div>
+                            <div className="display-checkbox">
+                                <CustomCheckbox title="Sat" checked={ this.state.Sat } onChange={(e)=> this.handleChangeValue("Sat", e.target.checked) }/>
+                            </div>
+
+                        </div>
                     </div>
-                    <div className="block-conteiner-two">
-                        <div>
-                            <CustomLabel>City</CustomLabel>
-                            <CustomInput onChange={ (e)=> this.handleChangeValue("City", e.target.value) } value={ this.state.City }/>
-                        </div>
-
-                        <div className="display-checkbox">
-                            <CustomRadioButton title="Aways" checked={ this.state.Aways } onChange={(e)=> this.handleChangeValue("Aways", e.target.checked) } />
-                        </div>
-                        <div className="display-checkbox">
-                            <CustomRadioButton title="Sometimes" checked={ this.state.Sometimes } onChange={(e)=> this.handleChangeValue("Sometimes", e.target.checked) }/>
-                        </div>
-                        <div className="display-checkbox">
-                            <CustomRadioButton title="Never" checked={ this.state.Never } onChange={(e)=> this.handleChangeValue("Never", e.target.checked) }/>
-                        </div>
-
-                        <CustomLabel>Days of the week</CustomLabel>
-                        <div className="display-checkbox">
-                            <CustomCheckbox title="Sun" checked={ this.state.Sun } onChange={(e)=> this.handleChangeValue("Sun", e.target.checked) }/>
-                        </div>
-                        <div className="display-checkbox">
-                            <CustomCheckbox title="Mon" checked={ this.state.Mon } onChange={(e)=> this.handleChangeValue("Mon", e.target.checked) }/>
-                        </div>
-                        <div className="display-checkbox">
-                            <CustomCheckbox title="Tue" checked={ this.state.Tue } onChange={(e)=> this.handleChangeValue("Tue", e.target.checked) }/>
-                        </div>
-                        <div className="display-checkbox">
-                            <CustomCheckbox title="Wed" checked={ this.state.Wed } onChange={(e)=> this.handleChangeValue("Wed", e.target.checked) }/>
-                        </div>
-                        <div className="display-checkbox">
-                            <CustomCheckbox title="Thu" checked={ this.state.Thu } onChange={(e)=> this.handleChangeValue("Thu", e.target.checked) }/>
-                        </div>
-                        <div className="display-checkbox">
-                            <CustomCheckbox title="Fri" checked={ this.state.Fri } onChange={(e)=> this.handleChangeValue("Fri", e.target.checked) }/>
-                        </div>
-                        <div className="display-checkbox">
-                            <CustomCheckbox title="Sat" checked={ this.state.Sat } onChange={(e)=> this.handleChangeValue("Sat", e.target.checked) }/>
-                        </div>
-
-                    </div>
-                </div>
-            </React.Fragment>
+                </React.Fragment>
+            </LoadingOverlay>
         )
     }
 }
